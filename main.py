5eb48bb953f65a351100a9ba010aafcb430b183b
Enok <157728443+Just3n0k@users.noreply.github.com>
2026-01-13 05:59:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.evniron.get("PORT",10000))
+    port = int(os.environ.get("PORT",10000))
 
     app.run(debug=False, port=port, host= "0.0.0.0")
+
 
 
